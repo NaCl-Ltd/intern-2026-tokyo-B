976b176c2b17c19aa5b287d504cab8fa21336577
Niiyama Keijiro <m22120@g.metro-cit.ac.jp>
2026-09-01 11:26:16 +0900

    ニックネーム機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :nickname, presence: true, uniqueness: true
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
